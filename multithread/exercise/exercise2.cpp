9f560458110639a0756a1036c9a87f954f8e35a7
刘一铭 <liuyiming@s-MacBook-puro.fios-router.home>
2017-12-05 13:03:49 -0500

    exercise 2 init

---
+++ multithread/exercise/exercise2.cpp
@@ -0,0 +1,45 @@
+/*************************************************************************
+	> File Name: exercise1.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Mon Dec  4 22:13:16 2017
+ ************************************************************************/
+
+#include <boost/date_time/posix_time/posix_time.hpp> 
+#include <boost/cstdint.hpp> 
+#include <iostream> 
+#include <boost/thread.hpp>
+#include <vector>
+
+void partial_sum(int st, int ed, boost::uint64_t& total){
+    for (int i = st; i < ed; ++i)
+        total += i;
+}
+
+int main() 
+{ 
+    int max_thread_num = boost::thread::hardware_concurrency();
+    std::vector<boost::thread> vec_t;
+    std::vector<boost::uint64_t> vec_sum(max_thread_num, 0);
+    int gap = 1000000000 / max_thread_num;
+
+    boost::posix_time::ptime start = boost::posix_time::microsec_clock::local_time(); 
+
+    for( int i=0; i<max_thread_num; ++i ){
+        vec_t[i] = boost::thread(partial_sum, 0, 500000000, boost::ref(vec_sum[i]));
+    }
+    
+    for (int i=0; i<max_thread_num; ++i){
+        vec_t[i].join();
+    }
+
+    boost::uint64_t sum = 0;
+    for (int i=0; i<max_thread_num; ++i)
+        sum += vec_sum[i];
+
+    boost::posix_time::ptime end = boost::posix_time::microsec_clock::local_time(); 
+    std::cout << "Multi-thread execution time:" << end - start << std::endl; 
+
+    std::cout << "Multi-thread execution result:" << sum << std::endl; 
+   
+} 
